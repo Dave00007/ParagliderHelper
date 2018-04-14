33cb07581810d9a0e5b5f46159e82fdd872ee848
DawidBalicki <dawidbalicki95@gmai.com>
2018-04-15 01:30:08 +0200

    Added AboutAppPage activity and buttons handler

--- app/src/main/java/com/application/paragliderhelper/WelcomePage.java
+++ app/src/main/java/com/application/paragliderhelper/WelcomePage.java
@@ -1,19 +1,36 @@
 package com.application.paragliderhelper;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class WelcomePage extends AppCompatActivity {
+public class WelcomePage extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
-
+        Button settingsButton = findViewById(R.id.settingsButton);
+        Button aboutAppButton = findViewById(R.id.aboutAppButton);
+        settingsButton.setOnClickListener(this);
+        aboutAppButton.setOnClickListener(this);
     }
 
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.settingsButton:
+                Intent intentSettingsPage = new Intent(this, SettingsPage.class);
+                startActivity(intentSettingsPage);
+                break;
+
+            case R.id.aboutAppButton:
+                Intent intentAboutApp =new Intent(this, AboutAppPage.class);
+                startActivity(intentAboutApp);
+                break;
+        }
+    }
 }
